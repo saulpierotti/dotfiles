9f50e6546032676ac9e43c5b8c5184e58670b49d
Saul Pierotti <saul@ebi.ac.uk>
2022-01-28 16:37:15 +0000

    created branch mac_os

--- acer_aspire_5/archlinux/root/home/saul/.screenlayout/ankara_dual_minitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 1366x768 --pos 1280x256 --rotate normal --output HDMI1 --mode 1280x1024 --pos 0x0 --rotate normal --output VIRTUAL1 --off
--- acer_aspire_5/archlinux/root/home/saul/.screenlayout/virtual_screen_attached.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --addmode VIRTUAL1 1024x768
-xrandr --output eDP1 --primary --mode 1366x768 --pos 1280x256 --rotate normal --output HDMI1 --off --output VIRTUAL1 --mode 1024x768 --pos 0x0 --rotate normal
