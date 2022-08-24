d16c0d96e0fb22ea244d71c4fffb4b59a88d395d
Saul Pierotti <saulpierotti@gmail.com>
2022-08-24 07:06:03 +0000

    initial

--- .profile
+++ .profile
@@ -1,1 +1,3 @@
+export PATH="${PATH}:/root/.scripts"
 alias v="vim"
+alias gitup="git_update.sh"
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 # install packages
+apk update
+apk upgrade
+apk add bash
 apk add openssh # for git ssh
 apk add git
 
 # clone my repos
 git clone https://github.com/saulpierotti/scripts.git
-mv scripts .scripts
+mv scripts $HOME/.scripts
 
 # symlinks for dotfiles
-ln -s $HOME/.profile $HOME
+ln -sf $HOME/.profile $HOME
